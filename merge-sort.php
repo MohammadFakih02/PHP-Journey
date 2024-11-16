a6a12e1f063bb06eaf6e1bf4a42656c6d72aed00
MohammadFakih02 <mohammadfakih025@gmial.com>
2024-11-16 15:14:59 +0200

    added merge sort

---
+++ merge-sort.php
@@ -0,0 +1,46 @@
+<?php
+
+header('Content-Type: application/json');
+$input = file_get_contents('php://input');
+$data = json_decode($input, true);
+
+if(isset($data['array'])){
+    $array = $data['array'];
+}
+function merge($left,$right){
+    $res = [];
+    $i=$j=0;
+    while($i < count($left) && $j < count($right)){
+        if($left[$i] < $right[$j]){
+            $result[]=$left[$i];
+            $i++;
+        }else{
+            $result[] = $right[$j];
+            $j++;
+        }
+    }
+
+    while($i<count($left)){
+        $result[] = $left[$i];
+        $i++;
+    }
+
+    while ($j < count($right)) {
+        $result[] = $right[$j];
+        $j++;
+    }
+    
+    return $result;
+}
+
+function mergesort($array){
+    if (count( $array ) <=1){
+        return $array;
+    }
+    $mid = floor(count( $array ) /2);
+    $left  = array_slice( $array,0, $mid );    
+    $right= array_slice( $array, $mid );
+    $left=mergesort($left);
+    $right = mergesort($right);
+    return merge($left, $right);
+}
